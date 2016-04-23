ed663dbe000018bb04f142415b69a062421ad00f
yisu <yisu@taobao.com>
2016-04-23 23:36:39 +0800

    moved webapp dir

--- src/dnspod/dnspod.go
+++ src/dnspod/dnspod.go
@@ -9,16 +9,16 @@ import (
 
 func Update(client *http.Client, cname string) string {
 	body := url.Values{
-		"login_email":    {login_email},
-		"login_password": {login_password},
-		"format":         {format},
-		"domain_id":      {domain_id},
-		"record_id":      {record_id},
-		"sub_domain":     {sub_domain},
-		"record_type":    {record_type},
-		"record_line":    {record_line},
-		"value":          {cname},
-		"ttl":            {ttl},
+		// "login_email":    {login_email},
+		// "login_password": {login_password},
+		// "format":         {format},
+		// "domain_id":      {domain_id},
+		// "record_id":      {record_id},
+		// "sub_domain":     {sub_domain},
+		// "record_type":    {record_type},
+		// "record_line":    {record_line},
+		// "value":          {cname},
+		// "ttl":            {ttl},
 	}
 	req, err := http.NewRequest("POST", "https://dnsapi.cn/Record.Modify", strings.NewReader(body.Encode()))
 	req.Header.Set("Accept", "text/json")
